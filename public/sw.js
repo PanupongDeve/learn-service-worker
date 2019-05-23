9f5f07d3fc3ecaf49a9dfd3a08eb7d7bed6afeeb
Panupong <5635512110@psu.ac.th>
2019-05-23 19:00:59 +0700

    update dynamic caches

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,13 @@
+const STATIC_CHACHES_NAME = 'static-v3';
+const DYNAMIC_CHACHES_NAME = 'dynamic';
+
 
 //  event เมื่อ ติดตั้ง service worker
 self.addEventListener('install', (event) => {
     // console.log('[Service Worker] Installing Service Worker ...', event);
     // add cache api
     event.waitUntil(
-        caches.open('static')
+        caches.open(STATIC_CHACHES_NAME)
             .then((cache) => {
                 console.log('[Service Worker] Pre caching App Shell');
                 cache.addAll([
@@ -31,6 +34,17 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
     // console.log('[Service Worker] Activating Service Worker ...', event);
+    event.waitUntil(
+        caches.keys()
+            .then((keyList) => {
+                return Promise.all(keyList.map((key => {
+                    if (key !== STATIC_CHACHES_NAME && key !== DYNAMIC_CHACHES_NAME) {
+                        console.log('[Serice worker] Removing old cache.', key);
+                        return caches.delete(key);
+                    }
+                })))
+            })
+    );
     return self.clients.claim();
 });
 
@@ -44,9 +58,19 @@ self.addEventListener('fetch', (event) => {
                 if(response) {
                     return response;
                 } else {
-                    return fetch(event.request);
+                    return fetch(event.request)
+                        .then((res) => {
+                            // open dynamic caches
+                            return caches.open(DYNAMIC_CHACHES_NAME)
+                                .then((cache) => {
+                                    cache.put(event.request.url, res.clone());
+                                    return res;
+                                })
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
                 }
             })
     );
 });
-
